8569c909eeb0ea2a4cbef2d7a5c45629eea7e4e7
Joshua Auriemma <allocate@gmail.com>
2014-08-16 15:46:33 -0400

    Implement initial modal logic

--- app/views/haiku.blade.php
+++ app/views/haiku.blade.php
@@ -3,7 +3,9 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>The HTML5 Herald</title>
+		<title>Stare Haikus</title>
+		<link rel="stylesheet" href="{{asset('css/foundation.css')}}" />
+		<script src="{{asset('/js/vendor/modernizr.js')}}"></script>
 		<style type="text/css">
 			html
 			{
@@ -25,7 +27,7 @@
 				text-align: center;
 				width: 99%;
 				top: 45%;
-				transform: translateY(-50%);
+				transform: translateY(-19%);
 			}
 
 			div {
@@ -35,8 +37,10 @@
 
 			p.haiku {
 				font-size: 5vw;
+				text-align: center;
 				padding-bottom: 0px;
 				line-height: 10px;
+				margin-bottom: 5vw;
 			}
 
 			p.casename {
@@ -104,19 +108,26 @@
 				font-size: 1.5vw;
 			}
 
+			table {
+				border: 0px;
+			}
 
 		</style>
-		<meta name="description" content="The HTML5 Herald">
-		<meta name="author" content="SitePoint">
-
-		<link rel="stylesheet" href="css/styles.css?v=1.0">
 
 		<!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 
+
+
 	</head>
 	<body>
+		<script src="{{ asset('js/vendor/jquery.js') }}"></script>
+		<script src="{{ asset('js/vendor/fastclick.js') }}"></script>
+		<script src="{{ asset('js/foundation.min.js') }}"></script>
+  <!-- Other JS plugins can be included here -->
+
+
 	<table align="center">
 		<tr>
 			<td>
@@ -126,23 +137,50 @@
 			<p class="casename">{{ $shortname }} ({{ $year }})</p><br />
 			</td>
 			<td class="voting">{{ Form::open(['route' => 'haiku.up']) }}{{ Form::hidden('id', $id) }}
-			<div class="upArrow">{{ Form::submit('', array('class' => 'arrow')) }}{{ Form::close() }}</div>
+			<div class="upArrow" align="center">{{ Form::submit('', array('class' => 'arrow')) }}{{ Form::close() }}</div>
 			@if ($voted != 'none')
-			<img class="gavel" src="{{ asset('images/gavel-lit.png') }}">
+			<img class="gavel" src="{{ asset('images/gavel-lit.png') }}" align="center">
 			@else
-			<img class="gavel" src="{{ asset('images/gavel.png') }}">
+			<img class="gavel" src="{{ asset('images/gavel.png') }}" align="center">
 			@endif
 			@if ($id != '31337')
-			<div class="downArrow">{{ Form::open(['route' => 'haiku.down']) }}{{ Form::hidden('id', $id) }}{{ Form::submit('', array('class' => 'arrow')) }}{{ Form::close() }}</div>
+			<div class="downArrow" align="center">{{ Form::open(['route' => 'haiku.down']) }}{{ Form::hidden('id', $id) }}{{ Form::submit('', array('class' => 'arrow')) }}{{ Form::close() }}</div>
 			@endif
 			</td>
 		</tr>
 		<tr>
+		<tr>
 			<td>
-			</td>
+			<span data-tooltip class="has-tip" data-options="show_on:large" title="Large Screen Sizes">show on</span>
+			</tr>
+			<tr>
 			<td>{{ Form::open(['route' => 'haiku.remove']) }}{{ Form::hidden('id', $id) }}{{ Form::submit('[x]', array('class' => 'remove')) }}{{ Form::close() }}
 			</td>
+			<tr>
+			<td>
+						<a href="#" data-reveal-id="myModal">Click Me For A Modal</a> 
+			<div id="myModal" style="display: none" class="reveal-modal" data-reveal>Awesome. I have it.<p class="lead">Your couch. It is mine.</p> <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p> <a class="close-reveal-modal">&#215;</a> </div>	
+			</td>
+			<td></td>
+			<tr><td><button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="right" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." data-original-title="" title="">Popover on right</button></td></tr>
+		</tr>
 
 	</table>
+
+<script>
+$(document).foundation();
+
+$('[data-toggle="popover"]').popover();
+
+$('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
+</script>
 	</body>
 </html>
